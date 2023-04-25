049a91f2a48a73f1bf75fc5597e274abe31838d7
seba <scativa@gmail.com>
2023-04-25 00:03:52 -0300

    Mongo DB Test

--- back/testDB.py
+++ back/testDB.py
@@ -2,6 +2,10 @@ from cgi import test
 import pymongo
 from pymongo.server_api import ServerApi
 
+# para probar si el puerto está abierto
+# curl http://portquiz.net:27017
+
+
 username = "ppca_cnea"
 password = "gadolineo1982"
 cluster = "cluster0.4r99gz4"
@@ -10,19 +14,36 @@ cluster = "cluster0.4r99gz4"
 
 mongoURL = f"mongodb+srv://{username}:{password}@{cluster}.mongodb.net/?retryWrites=true&w=majority"
 
+def mongodb_connect(mongoURL):
+    try:
+        client = pymongo.MongoClient(mongoURL,
+        server_api=ServerApi('1'),
+        serverSelectionTimeoutMS=100
+        )
 
-client = pymongo.MongoClient(mongoURL, 
-    server_api=ServerApi('1'),
-#    serverSelectionTimeoutMS=1
-    )
+        client.server_info() # force connection on a request as the
+                            # connect=True parameter of MongoClient seems
+                            # to be useless here 
 
-client.close()
+        return client
+    except pymongo.errors.ConnectionFailure as err:
+         print("Failed to connect to server {}".format(mongoURL))
+         print(err)
 
-print(mongoURL)
+    except pymongo.errors.ServerSelectionTimeoutError as err:
+        # do whatever you need
+        print(err)         
 
-db = client['database']
-coll = db['collection']
-print(coll)
+if __name__ == "__main__":
+    client = mongodb_connect(mongoURL)
 
-test = client.test
-# print(test)
+    client.close()
+
+    print(mongoURL)
+
+    db = client['database']
+    coll = db['collection']
+    print(coll)
+
+    test = client.test
+    # print(test)
